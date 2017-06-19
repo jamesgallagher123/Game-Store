2d5d263bf9164ccd78efed345639a844a8303fd5
Fraser <frasers@hotmail.co.uk>
2017-06-19 15:45:53 +0000

    Merge branch 'master' of https://github.com/jamesgallagher123/Game-Store into RyanBranch
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/scala/Customer.scala
+++ src/main/scala/Customer.scala
@@ -1,5 +1,5 @@
-abstract class Customer{
-  val customerID: String
-  val fullName: String
-  val points: Int
+class Customer(i: Int, fn: String, point: Int) extends Person{
+  val id: Int = i
+  val fullName: String = fn
+  val points: Int = point
 }
