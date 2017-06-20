bc8f42fc0729dc383d7655a898713c34a5343240
Fraser <frasers@hotmail.co.uk>
2017-06-20 11:05:02 +0000

    readded tostring override

--- src/main/scala/Hardware.scala
+++ src/main/scala/Hardware.scala
@@ -8,4 +8,5 @@ class Hardware (i: Int, fn: String, p: Double, q: Int) extends Items{
   val price: Double = p
   val quantity: Int = q
 
+  override def toString = s"Hardware | ID: $id | Full Name: $fullName | Price: $price | Quantity in Stock: $quantity"
 }
