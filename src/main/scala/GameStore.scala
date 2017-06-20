5ced634e248833ffa7a75280da07300e268bf389
Fraser <frasers@hotmail.co.uk>
2017-06-20 15:37:10 +0000

    updates

--- src/main/scala/GameStore.scala
+++ src/main/scala/GameStore.scala
@@ -84,14 +84,17 @@ class GameStore {
 
 
   def printReceipt(receipt: Receipt, floorStaff: FloorStaff, paidWithPoints: Boolean): Double = {
+    var a: Double = 0
     receipt.items.foreach(i =>println(s"ID: ${i.id} | Product: ${i.fullName} | Quantity: ${i.quantity} | Total Price: ${i.price*i.quantity}"))
+    receipt.items.foreach(i => a += i.price * i.quantity)
     println("You have been served by " + floorStaff.fullName)
     if (paidWithPoints.equals(true)) {
       println(s"You have paid with points")
+      println(s"You have saved: £$a")
     }
-    var a: Double = 0
-    receipt.items.foreach(i => a += i.price*i.quantity)
-    println(s"Total Order Price: $a")
+    else {
+      println(s"Total Order Price: £$a")
+    }
     a
   }
 }
