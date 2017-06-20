e62f1cee82c398df0cfea2dd7f462496bef29f6b
emmanuel haastrup <emmanuel.haastrup@gmail.com>
2017-06-20 13:09:26 +0100

    delete nonsense

--- src/main/scala/Games.scala
+++ src/main/scala/Games.scala
@@ -1,12 +1,12 @@
 /**
   * Created by Profile on 19/06/2017.
   */
-class Games(idc:Int, fnc:String, pricec:Double, quantityc:Int, rdc:java.util.Date) extends Items{
+class Games(idc:Int, fnc:String, pricec:Double, quantityc:Int, rdc:String) extends Items{
   override val id: Int = idc
   override val fullName: String = fnc
   override val price: Double = pricec
   override val quantity: Int = quantityc
-  val releaseDate: java.util.Date = rdc
+  val releaseDate: String = rdc
 
   override def toString = s"Game | ID: $id | Full Name: $fullName | Price: $price | Quantity in Stock: $quantity | Release Date: $releaseDate"
 }
--- src/main/scala/Receipt.scala
+++ src/main/scala/Receipt.scala
@@ -1,12 +1,10 @@
 import scala.collection.mutable.ListBuffer
 
-class Receipt(floorStaff: FloorStaff, customer: Customer, itemsList: ListBuffer[Items], date1: String) {
-  val floorStaffId: Int = floorStaff.id
-  val customerId: Int = customer.id
+class Receipt(itemsList: ListBuffer[Items], date1: String) {
   val items: ListBuffer[Items] = itemsList
-  val total: Double = calculatePrice(items)
   val date: String = date1
 
+  def total: Double = calculatePrice(items)
   def calculatePrice(items: ListBuffer[Items]): Double = {
     var total: Double = 0
     items.foreach(x => total += x.price * x.quantity)
