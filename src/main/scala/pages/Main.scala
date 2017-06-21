878f740def038518e4d7b67eb5f492a4c45e83af
Fraser <frasers@hotmail.co.uk>
2017-06-21 14:26:48 +0100

    Merge branch 'master' of https://github.com/jamesgallagher123/Game-Store into ryan-latest
    # Conflicts:
    #	src/main/scala/pages/Login.scala

--- src/main/scala/pages/Main.scala
+++ src/main/scala/pages/Main.scala
@@ -5,15 +5,19 @@ import scalafx.application.JFXApp.PrimaryStage
 
 object Main extends JFXApp {
   var loginWindow:LoginWindow = new LoginWindow
+  var floorStaffWindow:FloorStaffWindow = new FloorStaffWindow
   var mainStage:PrimaryStage = new PrimaryStage
 
-  setWindow("login")
+  setWindow("floorstaff")
   // val close:Button = new Button("Close"){relocate(x, y))
   // close.onAction = (e:ActionEvent) => {}
   // import scalafx.Include._
   def setWindow(window:String): Unit = {
     window match {
       case "login" => mainStage.scene = loginWindow
+        mainStage.width = 400
+        mainStage.height = 350
+      case "floorstaff" => mainStage.scene = floorStaffWindow
         mainStage.width = 400
         mainStage.height = 350
       case _ => println("Window not valid")
