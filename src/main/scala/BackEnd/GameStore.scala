aedfd1e851509c65da85cd35cc0af266a464b873
Fraser <frasers@hotmail.co.uk>
2017-06-23 12:51:11 +0100

    Merge branch 'master' of https://github.com/jamesgallagher123/Game-Store into ryan-latest
    # Conflicts:
    #	src/main/scala/BackEnd/GameStore.scala
    #	src/main/scala/pages/Main.scala
    #	src/main/scala/pages/TransactionsWindow.scala
    #	src/test/java/GetDaysProfitTest.scala
    #	src/test/java/PayWithPointsTest.scala

--- src/main/scala/BackEnd/GameStore.scala
+++ src/main/scala/BackEnd/GameStore.scala
@@ -150,7 +150,7 @@ class GameStore {
       val b: String = "\nYou have been served by " + floorstaffName + "\n"
       c = a + b
     }
-    if (paidWithPoints.equals(true)) {
+    else if (paidWithPoints.equals(true)) {
       receipt.items.foreach(i => a += s"ID: ${i.id} | Product: ${i.fullName} | Quantity: $quantity | Total Price: ${i.price * quantity}")
       println(a)
       val b: String = "\nYou have been served by " + floorstaffName +  "\nYou have paid using points"
